849b6df86774876cf209fdbb920560e82b55bf6f
sb.hwang <sb.hwang@du-sung.com>
2024-12-17 15:53:04 +0900

    1. APP name 변경 2. icon 사이즈별 지정 옵션 추가. 3. 프로그램 icon 추가

--- main.py
+++ main.py
@@ -4,13 +4,39 @@ from ttkbootstrap.constants import *
 from PIL import Image, ImageTk
 from tkinterdnd2 import DND_FILES, TkinterDnD
 
-class Mainframe(TkinterDnD.Tk):
+class ICO_CNVER(TkinterDnD.Tk):
+    """
+    아이콘 변환 프로그램
+    이미지 파일을 드래그앤드롭 해서 ICO 파일로 변환하는 기능
+    
+    1. 이미지 입력 ZONE에 이미지를 끌어다 놓는다.
+    2. ICO 파일이 이미지 파일과 같은 경로에 생성된다. 
+    """
     def __init__(self, config):
+        """
+        프로그램 초기화 초기화 메소드
+        
+        Args:
+            self (ICO_CONVERTER Object)
+            config (dictionary) : 프로그램 설정값
+
+        Returns:
+            ICO_CONVERTER Object
+        """
         super().__init__()
         self.outputPath = "./output/"
         self.config = config  # 설정 값 저장
 
     def initDragAndDropZone(self):
+        """
+        드래그 앤 드랍 zone 설정 메소드
+        
+        드래그 앤 드랍기능으로 사용할 컨트롤러 설정을 진행한다.
+
+        Args:
+            self (ICO_CONVERTER Object)
+        Returns:
+        """
         # 파일 드롭 영역
         self.dnd_label = ttk.Label(
             self,
@@ -25,6 +51,7 @@ class Mainframe(TkinterDnD.Tk):
         self.dnd_label.drop_target_register(DND_FILES)
         self.dnd_label.dnd_bind('<<Drop>>', self.on_drop)
 
+    def initPreviewImageView(self):
         # image view
         self.image_label = ttk.Label(
             self, 
@@ -33,13 +60,36 @@ class Mainframe(TkinterDnD.Tk):
             background="white",
             anchor="center",
             width=20)
+        
         self.image_label.pack(side='right', fill="y", anchor="e", padx=20, pady=20)
 
     def showImageLoadingError(self, msg):
+        """
+        에러메시지 출력 메소드
+
+        에러메시지를 이미지 출력 컨트롤에 출력한다.
+        
+        Args:
+            self (ICO_CONVERTER Object)
+            msg (string)    : 에러메시지
+
+        Returns:
+        """
         self.image_label.config(text=msg, bootstyle="danger")
 
     def displayImage(self, target, file_path):
-        """이미지를 로드하고 출력"""
+        """
+        이미지 출력 메소드
+
+        이미지를 로딩하여 리사이즈 한 후 미리보기 컨트롤에 출력한다.
+        
+        Args:
+            self (ICO_CONVERTER Object)
+            target (Object)     : 이미지를 출력할 객체
+            file_path (string)  : 이미지 파일 경로
+
+        Returns:
+        """
         try:
             # Pillow로 이미지 로드
             image = Image.open(file_path)
@@ -55,10 +105,21 @@ class Mainframe(TkinterDnD.Tk):
             target.image = photo  # 참조 유지
 
         except Exception as e:
-            print(e)
             self.image_label.config(text=f"Error loading image: \r\n{e}")
 
     def on_drop(self, event):
+        """
+        <<DROP>>> 이벤트 처리 함수
+
+        파일 DROP 이벤트 발생할시 파일 경로 및 ICO 파일로 컨버팅을 실행한다.
+        
+        Args:
+            self (ICO_CONVERTER Object)
+            target (Object)     : 이미지를 출력할 객체
+            file_path (string)  : 이미지 파일 경로
+
+        Returns:
+        """
         # 드롭된 파일 경로 처리
         files = self.split_files(event.data)
         if len(files) > 1:
@@ -67,7 +128,7 @@ class Mainframe(TkinterDnD.Tk):
             file_path = files[0]
             print(file_path)
 
-            if not file_path.lower().endswith((".png", ".jpg", ".jpeg")):
+            if not file_path.lower().endswith((".png", ".jpg", ".jpeg", ".webp")):
                 self.showImageLoadingError("Invalid file type.\r\nPlease drop an image.")
                 return
             
@@ -76,30 +137,62 @@ class Mainframe(TkinterDnD.Tk):
             
     @staticmethod
     def split_files(file_string):
+        """
+        파일 목록 split 메소드
+
+        입력된 파일 목록을 split하여 배열로 반환한다.
+
+        Args:
+            self (ICO_CONVERTER Object)
+            target (Object)     : 이미지를 출력할 객체
+            file_path (string)  : 이미지 파일 경로
+
+        Returns:
+            파일 목록 배열(array)
+        """
         # 여러 파일이 드래그된 경우 처리
         return file_string.split()
     
     def run(self):
-        self.title("ICON CONVERTER")
+        """
+        앱 실행 메소드
+
+        Args:
+            self (ICO_CONVERTER Object)
+        Returns:
+        """
+        self.title("ICON CNVER")
         self.geometry("360x160")
         self.minsize(360,160)
         self.maxsize(360,160)
 
+        self.iconbitmap("./resource/icon_cnver.ico")
+
         self.style = ttk.Style(theme="solar")  # 다른 테마로 변경 가능
         
         self.initDragAndDropZone()
+        self.initPreviewImageView()
+
         # self.convPNGtoICO("./resource/space-station.png")
 
         # APP 실행
         self.mainloop()
     
     def convertICO(self, path):
+        """
+        앱 실행 메소드
+
+        Args:
+            self (ICO_CONVERTER Object)
+
+        Returns:
+        """
         directory, file_name = os.path.split(path)
         base_name, extension = os.path.splitext(file_name)
         img = Image.open(path)
-        img.save(f"{directory}\\{base_name}.ico")
+        img.save(f"{directory}\\{base_name}.ico", format="ICO", sizes=[(16, 16), (32, 32), (48, 48), (64, 64), (128, 128), (256, 256)])
 
 if __name__ == "__main__":
-    config = {"debug": True, "version": "1.0"}  # 설정 값
-    app = Mainframe(config)
+    config = {"debug": True, "version": "0.0.1"}  # 설정 값
+    app = ICO_CNVER(config)
     app.run()
